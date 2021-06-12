95e75d5cc9b3732b06ba3120be0132253d1bc438
djalburb <djalburb@gmail.com>
2021-06-11 23:33:43 -0600

    finalizado

---
+++ GymManager/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('.table').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "No hay datos disponibles",
+            "info": "Mostrando _START_ de _END_ de _TOTAL_ Registros",
+            "infoEmpty": "mostrando 0 de 0 de 0 Registros",
+            "infoFiltered": "(filtrar de _MAX_ total Registros)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Registros",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "No se encontraron coincidencias",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Seguiente",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
+    });
+
+
+    $('.cedula').mask('000-000000-0000A');
+    $('.telefono').mask('00000000');
+    $('.money').mask('########.00');
+    $('.decimal').mask('########.00');
+
+
+});
